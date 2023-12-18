a592858889907e9987142241e088ab6d7cb6b1b8
Artur <artur.bachynskiy@gmail.com>
2023-12-18 16:02:04 +0200

    up modal 2

--- src/components/FullVacancyModal.jsx
+++ src/components/FullVacancyModal.jsx
@@ -10,7 +10,7 @@ const FullVacancyModal = ({ visible, setVisible, vacancy }) => {
     return (
         <div className={visible ? 'vacancyModal active' : 'vacancyModal'} onClick={() => setVisible(false)}>
             <div className={visible ? 'vacancyModalContent active' : 'vacancyModalContent'} onClick={e => e.stopPropagation()}>
-                <div className='vacancy_header'>
+                <div className='full_vacancy_header'>
                     <span>
                         <img className='location_icon' src={location} alt="" width={ 15 } />
                         {vacancy.city}
@@ -18,11 +18,14 @@ const FullVacancyModal = ({ visible, setVisible, vacancy }) => {
                     <img onClick={() => setVisible(false)} className='closeIcon' src={closeIcon} alt="" width={22}/>
                 </div>
                 <img className='job_icon' src={jobIcon} alt="" width={ 25 } height={ 22 } />
-                <span className='vacancy_title'>{vacancy.title}</span> <br/>
-                Salary - <span className='vacancy_salary'>{vacancy.salary}</span>
+                <span className='full_vacancy_title'>{vacancy.title}</span> <br/>
+                Salary - <span className='full_vacancy_salary'>{vacancy.salary}</span>
+                <div className='full_vacancy_text'>
+                    {vacancy.text}
+                </div>
                 <p>{vacancy.option1}</p>
                 <p>{vacancy.option2}</p>
-                <button className='button' onClick={() => navigate('/contact')}>Apply Now</button>
+                <button className='full_vacancy_button' onClick={() => navigate('/contact')}>Apply Now</button>
             </div>
         </div>
     );
--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -3,7 +3,7 @@ import location from '../images/location-pin-svgrepo-com.svg'
 import jobIcon from '../images/employee-id-svgrepo-com.svg'
 import FullVacancyModal from "./FullVacancyModal";
 
-const JobList = ({vacancies, country}) => {
+const JobList = ({ vacancies, country }) => {
     const [visible, setVisible] = useState(false)
     const [activeVacancy, setActiveVacancy] = useState({})
 
@@ -19,21 +19,21 @@ const JobList = ({vacancies, country}) => {
             }
             <div className='jobs_inner'>
                 {vacancies.map(vacancy =>
-                    <div className='job_item' key={vacancy.id}>
+                    <div className='job_item' key={ vacancy.id }>
                         <div className='vacancy_header'>
                             <span>
-                                <img className='location_icon' src={location} alt="" width={ 15 } />
-                                {vacancy.city}
+                                <img className='location_icon' src={ location } alt="" width={ 15 } />
+                                { vacancy.city }
                             </span>
-                            <span className='vacancy_salary'>{vacancy.salary}</span>
+                            <span className='vacancy_salary'>{ vacancy.salary }</span>
                         </div>
-                        <img className='job_icon' src={jobIcon} alt="" width={ 25 } height={ 22 } />
-                        <span className='vacancy_title'>{vacancy.title}</span>
+                        <img className='job_icon' src={ jobIcon } alt="" width={ 25 } height={ 22 } />
+                        <span className='vacancy_title'>{ vacancy.title }</span>
                         <button className='apply_button' onClick={() => showFullVacancyModal(vacancy)}>Show Details</button>
                     </div>
                 )}
-                <FullVacancyModal visible={ visible } setVisible={ setVisible } vacancy={ activeVacancy } />
             </div>
+            <FullVacancyModal visible={ visible } setVisible={ setVisible } vacancy={ activeVacancy } />
         </>
     );
 };

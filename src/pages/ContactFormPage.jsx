d93e3b0abb405213b69a71d25f64547757b04691
Artur <artur.bachynskiy@gmail.com>
2023-12-19 01:34:31 +0200

    add emailJS

--- src/pages/ContactFormPage.jsx
+++ src/pages/ContactFormPage.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import emailjs from '@emailjs/browser';
 
 const ContactFormPage = () => {
+    const formRef = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_q5lrsah', 'template_2gwu4tc', formRef.current, 'DZR5pkJsOc_DlCCIQ')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset()
+    }
+
     return (
         <div className='contact_page'>
-            <form className='contacts_form'>
+            <form onSubmit={sendEmail} ref={formRef} className='contacts_form'>
                 <h1>Fill out the application and we will call you!</h1>
-                <input className='input' type="text" placeholder='Full Name'/>
-                <input className='input' type="email" placeholder='Email'/>
+                <input name='user_name' className='input' type="text" placeholder='Full Name'/>
+                <input name='user_email' className='input' type="email" placeholder='Email'/>
                 <input className='input' type="text" placeholder='Phone Number'/>
-                <textarea className='input' name="" id="" cols="50" rows="3" placeholder='What a vacancy you liked?'></textarea>
+                <textarea style={{maxWidth: '300px', maxHeight: '100px'}} className='input' name="" id="" cols="30" rows="3" placeholder='What a vacancy you liked?'></textarea>
                 <button className='submit_contact_btn button'>Send Contacts</button>
             </form>
         </div>

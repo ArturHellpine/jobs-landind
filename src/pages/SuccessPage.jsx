c8990d6a753d0c99a0d153f927fb5faff5559fd8
Artur <artur.bachynskiy@gmail.com>
2023-12-25 19:39:58 +0200

    fixed intro items

--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {useNavigate} from "react-router-dom";
-import {Result} from "antd";
+import { useNavigate } from "react-router-dom";
+import { Result } from "antd";
 import selectIcon from '../images/select-svgrepo-com.svg'
 
 const SuccessPage = () => {
@@ -8,11 +8,11 @@ const SuccessPage = () => {
 
     return (
         <div className='success_page'>
-            <Result status={"success"} style={{marginBottom: '-50px'}} />
+            <Result status={ "success" } style={{ marginBottom: '-50px' }} />
             <h2 className='success_text'>Заявка отправлена! <br/> Мы ответим вам в течение дня</h2>
             <button className='success_btn button' onClick={() => navigate('/home')}>
                 <span>Закрыть</span>
-                <img src={selectIcon} className='selectIcon' alt=""/>
+                <img src={ selectIcon } className='selectIcon' alt=""/>
             </button>
         </div>
     );
